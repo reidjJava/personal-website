b19df2e070d2988e68c67d6edfe4843d0d51945b
Reidj3 <sp1.ke@mail.ru>
2020-05-01 13:17:49 +0300

    + spring

---
+++ src/main/java/ru/reidj/cardsite/controller/WebController.java
@@ -0,0 +1,12 @@
+package ru.reidj.cardsite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
